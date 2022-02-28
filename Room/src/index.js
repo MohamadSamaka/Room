9fbd414f53df167e259ae2089449e04fbffc0bf1
TheXJoKeR71 <mhmd_dragon1@hotmail.com>
2022-03-01 01:01:29 +0200

    cleaned up and changed the way i readnder stuff , much cleaner now

--- Room/src/index.js
+++ Room/src/index.js
@@ -1,77 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Wrapper from './Wrapper'
+import App from './App'
 import reportWebVitals from './reportWebVitals';
-// import Header from './Header.js';
-// import Main from './Main.js';
-// import Footer from './Footer.js';
-import ProductFullInfo from './FullProductDescription'
-// import hell from '.Translation'
-// let ProductFullInfoContainer = document.createElement('div');
-// ProductFullInfoContainer.classList.add("product-full-info", "disable");
-// document.body.insertBefore(ProductFullInfoContainer, document.getElementsByClassName("wrapper")[0]); //inserting the ProductFullInfoContainer before the wrapepr elem
+import { BrowserRouter as Router} from "react-router-dom";
 
-// import Something from './Translation'
-// import {useTranslation } from "react-i18next";
-let wrapper = document.createElement("div");
-wrapper.classList.add("wrapper");
-wrapper.classList.add("active");
-
-// const e = React.createElement;
-// ReactDOM.render(
-//   e('div', {className: "wrapper active"}, 'Hello World'),
-//   document.body
-// );
-
-// const Index = () => {
-//   return(
-//     <>
-//       <div className="product-full-info disable">
-//         <ProductFullInfo></ProductFullInfo>
-//       </div>
-//       <div className="wrapper active">
-//         <Wrapper></Wrapper>
-//       </div>
-//   </>
-//   );
-// };
-
+// import ProductFullInfo from './FullProductDescription'
 
 // ReactDOM.render(
-//   Index,
-//   document.body
-// );
-
-// export default Index;
-
-// const HelloWorld = () => {
-//   return (
-//     <p>CMOOON WORK FOR GOD SAKE</p>
-//   );
-// };
-
-// ReactDOM.render(
-//   HelloWorld,
-//   document.body
-// );
-
-// export default HelloWorld;
-
+//   <>
+//   <ProductFullInfo/>
+//   <div className='wrapper active'>
+//   <Wrapper/>
+//   </div>
+//   </>,
+//   document.getElementById("wrapper")
+// )
 
 ReactDOM.render(
-  <ProductFullInfo/>,
-  document.body
-);
-
-document.body.appendChild(wrapper)
-ReactDOM.render(
-  <Wrapper/>,
-  wrapper
-);
-
-
+  <Router>
+    <App></App>
+  </Router>,
+  document.getElementById("main-wrapper")
+)
 
 
 // If you want to start measuring performance in your app, pass a function
