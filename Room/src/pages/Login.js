d4b022d9496b57154157b2cd26f68f9526f3f1fe
TheXJoKeR71 <mhmd_dragon1@hotmail.com>
2022-03-03 10:17:01 +0200

    improving a Login page , im struggling with the line decoration thingy i will work on it later

--- Room/src/pages/Login.js
+++ Room/src/pages/Login.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import {React, useEffect} from 'react'
 import '../styles/Login.css'
+import {useNavigate} from 'react-router-dom';
+
 export default function Login(){
+    let navigate = useNavigate();
+
+    useEffect(()=>{
+        document.body.classList=[];
+    }, [])
+    
     return(
     <>
-        <form id="login-wrapper"  method="POST">
-            <span>Sign in</span>
+        <form className="login-wrapper"  method="POST">
+            <h2 className="logo" onClick={()=>{navigate("/")}}>Logo</h2>
+            <h1>Welcome Back!</h1>
             <div className="user-input">
                 <input type="text" name="user" id="user" required></input>
                 <span>Userame</span>
-                <div className="line-decoration"></div>
+                {/* <div className="line-decoration"></div> */}
             </div>
             <div className="user-input">
                 <input type="password" name="pass" id="pass" required></input>
                 <span>Password</span>
-                <div className="line-decoration"></div>
+                {/* <div className="line-decoration"></div> */}
             </div>
             <input type="submit" value="Log in" id="submit" name="submit"></input>
             <a href="html/singup.html">not signed in?</a>
