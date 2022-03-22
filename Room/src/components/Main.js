9d0d3b1a22118f17803fdc642ab58ab4879acfef
thexjoker-desktop <mhmdkais20@gmail.com>
2022-03-22 20:49:05 +0200

    seperated things info mutiple files to organize it more with help of hooks

--- Room/src/components/Main.js
+++ Room/src/components/Main.js
@@ -1,56 +1,31 @@
-import React, {useEffect} from 'react'
+import {React, useEffect, useState, useContext } from 'react'
 import '../styles/Main.css'
-// import '../styles/FullProductDescription.css'
-// import background from './images/background.jpg'
-// import shipping from './images/shipping.png'
-// import return_ from './images/return.png'
-// import low_price from './images/low-price.png'
 import StarIcon from '@mui/icons-material/Star';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import {useTranslation } from "react-i18next";
-import '../Translation'
-import reactDom from 'react-dom';
-import Axios from 'axios';
+import '../Translation.js'
+import ProductBasicInfoContainer from '../bluePrints/ProductBasicInfoContainer';
+import {ApplicationContext} from '../App.js'
+
 
 /*eslint-disable*/
 function Main() {
     const {t} = useTranslation();
-    let Products;
-    useEffect(()=>{
-        Axios.get('http://localhost:3001/api/products')
-        .then((res) => {
-        Products = res.data.map(data => {
-            let images = JSON.parse(data["Images"]);
-            return <div className="product" key={data.Id} onClick={ShowProductDescription}>
-                        <div className="thumb">
-                                <img className="product-image"src={`http://localhost:3001/images/products/${images[0]}`}></img>
-                                <ul className='product-slider-op'>
-                                    <li>
-                                        <AddShoppingCartIcon className="add-to-cart"></AddShoppingCartIcon>
-                                    </li>
-                                    <li>
-                                        <FavoriteIcon className="add-to-wishlist"></FavoriteIcon>
-                                    </li>
-                                </ul>
-                        </div>
-                        <div className="product-basic-info">
-                            <h4 className="product-title">{data["Title"]}</h4>
-                            <span className="product-price">{data["Price"]}$</span>
-                        </div>
-                    </div>
-        })
-        reactDom.render(Products, document.getElementsByClassName('products-section')[0]);
-    });
-        console.log("hello this is from the Main!");
-    },[])
+    const currentProductsRawData = useContext(ApplicationContext)[0][0];
+    const setClickedProduct = useContext(ApplicationContext)[1][1];
+    const [productsElemenetContainer, setproductsElemenetContainer] = useState([]);
 
-    function ShowProductDescription() {
-        var DescElem = document.getElementsByClassName('product-full-info')[0];
-        var MainWrapper = document.getElementsByClassName('wrapper')[0];
-        DescElem.classList.replace('disable','active');
-        MainWrapper.classList.replace('active', 'inactive');
-    }
+    useEffect(()=>{
+        setproductsElemenetContainer(currentProductsRawData.map(data =>{
+            try{
+                data["Images"] = JSON.parse(data["Images"])
+            }
+            catch{
+                console.log("is either already been converted to json, or the can't be canverted in the first place")
+            }
+            return ProductBasicInfoContainer(data, setClickedProduct)
+        }))
+        console.log("hello this is from the Main!");
+    },[currentProductsRawData])
 
     return(
         <main id="main">
@@ -67,27 +42,11 @@ function Main() {
                         </div>
                     </div>
                 </div>
-                {/* <div className="features">
-                    <div id="shipping">
-                        <img src={shipping} alt="shipping icon"></img>
-                        <h3>Priority Shipping</h3>
-                        <p>We do our best to deliever your</p>
-                    </div>
-                    <div id="return">
-                        <img src={return_} alt="shipping icon"></img>
-                        <h3>Fuss Free Returns</h3>
-                        <p>best return policies.</p>
-                    </div>
-                    <div id="discount">
-                        <img src={low_price} alt="shipping icon"></img>
-                        <h3>Cheapest in country</h3>
-                        <p>We find the best prices for our customers.</p>
-                    </div>
-                </div> */}
                 <div className="shop">
                     <div id="shop-decoration"><b></b><span><StarIcon id ="star-icon"></StarIcon></span><b></b></div>
                     <h1 id="shop-title">{t('Main.featured-products')}</h1>
                     <div className="products-section">
+                        {productsElemenetContainer}
                     </div>
                 </div>
             </section>
