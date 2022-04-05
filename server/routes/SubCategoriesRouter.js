55c674b1b1314c7cc1deedd0200981e4061ded00
thexjoker-desktop <mhmdkais20@gmail.com>
2022-04-05 17:39:35 +0300

    fixing the names, it was nammed as category controller while this file is a subCategory controller

--- server/routes/SubCategoriesRouter.js
+++ server/routes/SubCategoriesRouter.js
@@ -3,10 +3,10 @@ const router = express.Router()
 const SubCategoriesController = require('../controllers/SubCategoriesController.js')
 
 router.route("/subCategories")
-.get(SubCategoriesController.GetAllCategories)
-.post(SubCategoriesController.AddCategory)
-.put(SubCategoriesController.UpdateCategory)
-.delete(SubCategoriesController.DeleteCategory)
+.get(SubCategoriesController.GetAllSubCategories)
+.post(SubCategoriesController.AddSubCategory)
+.put(SubCategoriesController.UpdateSubCategory)
+.delete(SubCategoriesController.DeleteSubCategory)
 
 
 module.exports = router;
